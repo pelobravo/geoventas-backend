95dd71c293bb236285203eacf8bd6fb9d3eacff7
pelobravo <pelobravo@gmail.com>
2026-05-21 12:12:47 -0400

    database.py

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "postgresql://postgres:KGmiXqQCMIAsYboUZkOsyWiHjVAVDGXg@kodama.proxy.rlwy.net:33483/railway"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
