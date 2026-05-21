9a86da14d951bd77280511435e309f448414b7fd
pelobravo <pelobravo@gmail.com>
2026-05-21 12:13:28 -0400

    models.py

--- models.py
+++ models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from database import Base
 
+class Cliente(Base):
+    __tablename__ = "clientes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    codigo = Column(String)
+    nombre = Column(String)
+    direccion = Column(String)
+    telefono = Column(String)
+    latitud = Column(Float)
+    longitud = Column(Float)
+    activo = Column(Boolean, default=True)
