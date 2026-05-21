5372d69a113171935a5677f976685e03f931bcb7
pelobravo <pelobravo@gmail.com>
2026-05-21 12:27:58 -0400

    database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 DATABASE_URL = "postgresql://postgres:KGmiXqQCMIAsYboUZkOsyWiHjVAVDGXg@kodama.proxy.rlwy.net:33483/railway"
 
