30cd876c10a8ee805916450689b9f28a2f2de9e9
pelobravo <pelobravo@gmail.com>
2026-05-21 12:21:19 -0400

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from database import engine, Base
 import models
+import pandas as pd
 
 Base.metadata.create_all(bind=engine)
 
@@ -13,3 +14,27 @@ def home():
 @app.get("/clientes")
 def clientes():
     return {"modulo": "clientes activo"}
+
+@app.post("/upload-csv")
+async def upload_csv(file: UploadFile = File(...)):
+    # Leer el archivo CSV subido
+    df = pd.read_csv(file.file)
+    
+    # Ejemplo: mostrar las primeras filas
+    datos = df.head().to_dict(orient="records")
+    
+    return {
+        "mensaje": "Archivo CSV procesado correctamente",
+        "filas": len(df),
+        "columnas": list(df.columns),
+        "vista_previa": datos
+    }
+
+@app.post("/upload-excel")
+async def upload_excel(file: UploadFile = File(...)):
+    df = pd.read_excel(file.file)
+    
+    return {
+        "columnas": list(df.columns),
+        "filas": len(df)
+    }
