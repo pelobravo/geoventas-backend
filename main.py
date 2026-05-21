79432c5ce9a5092faaa67ecafbfa96253bcd87bc
pelobravo <pelobravo@gmail.com>
2026-05-21 12:15:52 -0400

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from database import engine, Base
+import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"mensaje": "GeoVentas funcionando 🚀"}
+
+@app.get("/clientes")
+def clientes():
+    return {"modulo": "clientes activo"}
