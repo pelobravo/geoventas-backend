35dd53c0daa7773f1e21134b4d68fd110f28e9c7
pelobravo <pelobravo@gmail.com>
2026-05-21 14:55:09 -0400

    models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean
+from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
 from database import Base
+from datetime import datetime
 
 class Cliente(Base):
     __tablename__ = "clientes"
@@ -15,3 +16,24 @@ class Cliente(Base):
     frecuencia = Column(String)
     empresa = Column(String)
     activo = Column(Boolean, default=True)
+
+
+class RankingVendedor(Base):
+    __tablename__ = "ranking_vendedores"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    vendedor = Column(String)
+    cartera = Column(Float)
+    activacion = Column(Float)
+
+    facturas_bs = Column(Float)
+    pedidos_fact = Column(Float)
+
+    notas = Column(Float)
+    total = Column(Float)
+
+    drop_size_neto = Column(Float)
+    drop_size_cajas = Column(Float)
+
+    fecha_carga = Column(DateTime, default=datetime.utcnow)
